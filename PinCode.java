98fa110c823ad1de39f22cadcfd2a2b7e513dc0e
gitbash <kolhenagesh2017@gmail.com>
2021-05-20 15:52:40 +0530

    Added: Valid Pincode

--- PinCode.java
+++ PinCode.java
@@ -1,7 +1,23 @@
+	import java.util.regex.Pattern;
+	import java.util.regex.Matcher;
+
 public class PinCode {
+	    public static boolean isValid(String pincode) {
 
-   public static void main(String[] args){
+	      String ZIP = "[1-9]{1}[0-9]{5}";
 
-      System.out.println("Welcome");
-   }
+	      Pattern p = Pattern.compile(ZIP);
+
+	      if (pincode == null){
+	         return false;
+	      }
+
+	      Matcher m = p.matcher(pincode);
+	      return m.matches();
+	      }
+
+    	   public static void main(String args[]){
+	      String Pincode1 ="400708";
+	      System.out.println( Pincode1 +":" +isValid(Pincode1));
+	      }
 }
